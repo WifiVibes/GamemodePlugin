fcf68e3a3b10748fac1d509f74287782b623573d
WifiVibes <49114933+WifiVibes@users.noreply.github.com>
2020-09-16 19:20:42 +0200

    Update Main.java

--- bin/nl/wifivibes/gamemodepl/Main.java
+++ bin/nl/wifivibes/gamemodepl/Main.java
@@ -1,41 +1,42 @@
 package nl.wifivibes.gamemodepl;
 
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public enum GM {
+
+public class Main extends JavaPlugin implements Listener{
 	
-	CREATIVE,
-	SURVIVAL,
-	ADVENTURE,
-	SPECTATOR,
-	FLY;
 	
-	public static void changeGamemode(GM gamemode, Player p) {
-		switch(gamemode) {
-		case CREATIVE :
-			p.setGameMode(GameMode.CREATIVE);
-			p.sendMessage(ChatColor.GOLD + "Jouw gamemode is veranderd naar Creative!");
-			break;
-		case SURVIVAL :
-			p.setGameMode(GameMode.SURVIVAL);
-			p.sendMessage(ChatColor.GOLD + "Jouw gamemode is veranderd naar Survival!");
-			break;
-		case ADVENTURE :
-			p.setGameMode(GameMode.ADVENTURE);
-			p.sendMessage(ChatColor.GOLD + "Jouw gamemode is veranderd naar Adventure!");
-			break;
-		case SPECTATOR :
-			p.setGameMode(GameMode.SPECTATOR);
-			p.sendMessage(ChatColor.GOLD + "Jouw gamemode is veranderd naar Spectator!");
-			break;
-		case FLY :
-			p.setAllowFlight(true);
-			p.setFlying(true);
-			p.sendMessage(ChatColor.GOLD + "Vliegen aangedaan");
-			break;
-		}
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
-
+	
+	 @Override
+		public boolean onCommand(CommandSender sender, Command cmd, String CommandLabel, String[] args) {
+		 if (sender instanceof Player) {
+			 Player p = (Player) sender;
+			 if (cmd.getName().equalsIgnoreCase("fly")) {
+				 GM.changeGamemode(GM.FLY, p);
+				 return true;
+			 }
+			 if (cmd.getName().equalsIgnoreCase("gamemode")) {
+				 if (args.length == 1) {
+					 if (args[0].equalsIgnoreCase("1") || args[0].equalsIgnoreCase("c")) {
+						 GM.changeGamemode(GM.CREATIVE, p);
+					 }else if (args[0].equalsIgnoreCase("2") || args[0].equalsIgnoreCase("a")) {
+						 GM.changeGamemode(GM.ADVENTURE, p);
+					 }else if (args[0].equalsIgnoreCase("3") || args[0].equalsIgnoreCase("sp")) {
+						 GM.changeGamemode(GM.SPECTATOR, p);
+					 }else if (args[0].equalsIgnoreCase("0") || args[0].equalsIgnoreCase("s")) {
+						 GM.changeGamemode(GM.SURVIVAL, p);
+					 }
+				 }
+			 }
+		 }
+		 return false;
+	 }
 }
